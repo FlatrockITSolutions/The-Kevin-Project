4993f36915fb70ce02fce8fb7a72baa0ca612d2b
FlatrockDesigns <skippyflatrock@gmail.com>
2022-11-01 04:11:56 -0700

    used to install the dependencies for the bot.

---
+++ pip installs.bat
@@ -0,0 +1,8 @@
+python -m pip install -U pip
+pip install discord
+pip install pythonbible
+pip install python-dotenv
+
+pip install -U discord==1.7.3 pip install -U discord.py==1.7.3
+echo Install Complete!!!!
+pause
